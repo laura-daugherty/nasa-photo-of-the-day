73975ff3c2dc6c8f3d0e356d249dddbbfaf598f3
Laura Daugherty <lauradaugherty6@gmail.com>
2019-07-10 17:27:20 -0700

    WIP components rendering -data passing as expected -hero component added -header component initialized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios"
+import Hero from "./components/hero"
+import Header from "./components/header"
+
 import { setUseProxies } from "immer";
 
 function App() {
@@ -20,19 +23,17 @@ function App() {
     console.log("useEffect fetching Data")
     axios
       .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-04")
-      .then(result => setPicture(result.data.hdurl))
+      .then(result => setData(result.data))
         //where do I console.log() to see the data??
       .catch(err => console.log(err));
   }, [] );
 
-
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-      <img src={picture} alt="Nasa Picture Of the Day"/>
+      <Header />
+      <Hero 
+        data={data} 
+      />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <h1>
+      Nasa is a big deal!
+    </h1>
+  )
+}
+
+export default Header;
