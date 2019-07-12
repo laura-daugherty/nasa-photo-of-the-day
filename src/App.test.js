bb6bc3dca399ceeb3e277af2741a7e602d35fb28
Laura Daugherty <lauradaugherty6@gmail.com>
2019-07-11 20:26:32 -0700

    Semantic UI/Testing -adding dependancies -basic snapshot tests added -utilizing semantic UI for styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
+
 import App from './App';
+import Hero from "./components/hero"
+import Header from "./components/header"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('<App />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<Hero />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Hero />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<Heading />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Header />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
